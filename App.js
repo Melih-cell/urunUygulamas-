2f659fbfbb8b4d9155fd208a71333ba3c09134ff
Melih <81979457+Melih-cell@users.noreply.github.com>
2022-06-16 03:14:48 +0300

    update

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, Button, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box_1}>
+        <Text style={styles.card_title}>Hello Bebek</Text>
+        <Text>Hello</Text>
+      <TouchableOpacity  style={styles.card_button_container}>
+        <Text style={styles.card_button_container_title} >Melih Dokuzlar</Text>
+      </TouchableOpacity>
+      </View>
+    
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#871165'
   },
-});
+  box_1: {
+    backgroundColor: '#ff2f73',
+    margin: 100,
+    marginTop: 30,
+    borderWidth: 3,
+    padding: 5,
+    alignItems: 'center',
+    height: 200,
+    width: 400,
+    borderBottomLeftRadius: 7,
+    borderBottomRightRadius: 7
+  },
+  card_title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 10,
+    marginBottom: 5
+  },
+  card_button_container: {
+      backgroundColor: 'blue',
+      padding: 10,
+      borderBottomLeftRadius: 6,
+      borderBottomRightRadius: 6,
+      flex: 1,
+      overflow: 'hidden',
+      marginBottom: 0
+
+  },
+  card_button_container_title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'white',
+    }
+})
+
+export default App;

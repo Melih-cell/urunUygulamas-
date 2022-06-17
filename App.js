6007e58413a7b1b8769cadaaac42c4d1ea48a64e
Melih <81979457+Melih-cell@users.noreply.github.com>
2022-06-17 06:29:09 +0300

    comp || Patika

--- App.js
+++ App.js
@@ -1,60 +1,44 @@
-import React from 'react';
-import { Text, View, Button, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
+import Home from './src/components/Home';
+import data_json from './src/components/Data.json';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.box_1}>
-        <Text style={styles.card_title}>Hello Bebek</Text>
-        <Text>Hello</Text>
-      <TouchableOpacity  style={styles.card_button_container}>
-        <Text style={styles.card_button_container_title} >Melih Dokuzlar</Text>
-      </TouchableOpacity>
-      </View>
-    
+  const fonks = ({ item }) => <Home dat={item}/>
+  const keyEX = (item) => item.id.toString();
 
-    </SafeAreaView>
+  return(
+    <View style={styles.container}>
+      <View style={styles.still_container}>
+        <Text style={styles.title}>PATIKASTORE</Text>
+        <TextInput placeholder='    Ara...' style={styles.input}></TextInput>
+      </View>
+      <FlatList numColumns={2} data={data_json} renderItem={fonks} keyExtractor={keyEX}></FlatList>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#871165'
+    flex: 1
   },
-  box_1: {
-    backgroundColor: '#ff2f73',
-    margin: 100,
-    marginTop: 30,
-    borderWidth: 3,
-    padding: 5,
-    alignItems: 'center',
-    height: 200,
-    width: 400,
-    borderBottomLeftRadius: 7,
-    borderBottomRightRadius: 7
-  },
-  card_title: {
-    fontSize: 25,
+  title: {
+    color: '#800080',
     fontWeight: 'bold',
-    margin: 10,
-    marginBottom: 5
+    fontSize: 34,
   },
-  card_button_container: {
-      backgroundColor: 'blue',
-      padding: 10,
-      borderBottomLeftRadius: 6,
-      borderBottomRightRadius: 6,
-      flex: 1,
-      overflow: 'hidden',
-      marginBottom: 0
-
+  still_container: {
+    // alignItems: 'center'
+    margin: 15
   },
-  card_button_container_title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'white',
-    }
+  input: {
+    width: 575,
+    height: 30,
+    backgroundColor: '#eee',
+    borderRadius: 10,
+    
+  }
 })
+
+
 
 export default App;
